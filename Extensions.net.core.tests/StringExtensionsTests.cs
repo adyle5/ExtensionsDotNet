4b2b5ef74be004b28570d79a7bf3e68a57823892
Adrian <adyle5@hotmail.com>
2018-10-16 07:53:11 -0400

    Initial commit

---
+++ Extensions.net.core.tests/StringExtensionsTests.cs
@@ -0,0 +1,377 @@
+﻿using Extensions.net.strings;
+using System;
+using Xunit;
+
+namespace Extensions.net.core.tests.strings
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void IsNullOrWhiteSpace()
+        {
+            string str1 = "";
+            Assert.Equal(str1.IsNullOrWhiteSpaceExt(), string.IsNullOrWhiteSpace(str1));
+
+            string str2 = "Not whitespace";
+            Assert.Equal(str2.IsNullOrWhiteSpaceExt(), string.IsNullOrWhiteSpace(str2));
+
+            string str3 = " ";
+            Assert.Equal(str3.IsNullOrWhiteSpaceExt(), string.IsNullOrWhiteSpace(str3));
+        }
+
+        [Fact]
+        public void TestIsNullOrEmpty()
+        {
+            string str1 = "";
+            Assert.Equal(str1.IsNullOrEmptyExt(), string.IsNullOrEmpty(str1));
+
+            string str2 = " ";
+            Assert.Equal(str2.IsNullOrEmptyExt(), string.IsNullOrEmpty(str2));
+
+            string str3 = "Not empty";
+            Assert.Equal(str3.IsNullOrEmptyExt(), string.IsNullOrEmpty(str3));
+        }
+
+        [Fact]
+        public void Compare()
+        {
+            string str1 = "Apples";
+            string str2 = "Bananas";
+            Assert.Equal(str1.CompareExt(str2), string.Compare(str1, str2));
+            Assert.Equal(str2.CompareExt(str1), string.Compare(str2, str1));
+
+            string str3 = "apples";
+            Assert.Equal(str3.CompareExt(str1), string.Compare(str3, str1, true));
+
+            string str4 = null;
+            Assert.Equal(str1.CompareExt(str4), string.Compare(str1, str4));
+        }
+
+        [Fact]
+        public void CompareFirst()
+        {
+            string str1 = "Apples";
+            string str2 = "Bananas";
+            Assert.Equal("Apples", str1.CompareFirstExt(str2));
+
+            string str3 = "apples";
+            Assert.Equal("", str1.CompareFirstExt(str3));
+        }
+
+        [Fact]
+        public void CompareOrdinal()
+        {
+            string str1 = "Apples";
+            string str2 = "Bananas";
+            Assert.Equal(str1.CompareOrdinalExt(str2), string.CompareOrdinal(str1, str2));
+            Assert.Equal(str2.CompareOrdinalExt(str1), string.CompareOrdinal(str2, str1));
+
+            string str3 = "apples";
+            Assert.Equal(str3.CompareOrdinalExt(str1), string.CompareOrdinal(str3, str1));
+
+            string str4 = null;
+            Assert.Equal(str1.CompareOrdinalExt(str4), string.CompareOrdinal(str1, str4));
+        }
+
+        [Fact]
+        public void CompareOrdinal2()
+        {
+            string str1 = "Apples";
+            int str1Index = 0;
+            string str2 = "Bananas";
+            int str2Index = 1;
+            int length = 3;
+            Assert.Equal(str1.CompareOrdinalExt(str1Index, str2, str2Index, length), string.CompareOrdinal(str1, str1Index, str2, str2Index, length));
+            Assert.Equal(str2.CompareOrdinalExt(str2Index, str1, str1Index, length), string.CompareOrdinal(str2, str2Index, str1, str1Index, length));
+        }
+
+        [Fact]
+        public void Concatenate()
+        {
+            string str1 = "fu";
+            string str2 = "bar";
+            string str3 = "star";
+            Assert.Equal(str1.ConcatExt(str2, str3), string.Concat(str1, str2, str3));
+
+            string[] arr1 = { str2, str3 };
+            Assert.Equal(str1.ConcatExt(arr1), string.Concat(str1, str2, str3));
+
+            string[] arr2 = null;
+            Assert.Equal(str1.ConcatExt(arr2), string.Concat(str1, arr2));
+        }
+
+        [Fact]
+        public void Copy()
+        {
+            string str1 = "fubar";
+            Assert.Equal(str1.CopyExt(), string.Copy(str1));
+            Assert.Equal(str1.CopyExt(), string.Copy(str1));
+        }
+
+        [Fact]
+        public void Format()
+        {
+            string str1 = "fu{0}{1}";
+            string str2 = "bar";
+            string str3 = "star";
+
+            object obj1 = new object();
+
+            Assert.Equal(str1.FormatExt(str2, str3), string.Format(str1, str2, str3));
+            Assert.Equal(str1.FormatExt(str2, obj1), string.Format(str1, str2, obj1));
+
+            object obj2 = null;
+            Assert.Equal(str1.FormatExt(str2, obj2), string.Format(str1, str2, obj2));
+        }
+
+        [Fact]
+        public void Intern()
+        {
+            string str1 = "test";
+            Assert.Equal(str1.InternExt(), string.Intern(str1));
+        }
+
+        [Fact]
+        public void IsInterned()
+        {
+            string str1 = "test";
+            Assert.Equal(str1.IsInternExt(), string.IsInterned(str1));
+        }
+
+        [Fact]
+        public void Join()
+        {
+            string str1 = "fu";
+            string str2 = "bar";
+            string separator = " ";
+
+            Assert.Equal(str1.JoinExt(separator, new string[1] { str2 }, 0, 0), string.Join(separator, new string[2] { str1, str2 }, 0, 0));
+            Assert.Equal(str1.JoinExt(separator, str2), string.Join(separator, str1, str2));
+        }
+
+        [Fact]
+        public void ReferenceEqualsExt()
+        {
+            string str1 = "test";
+            string str2 = "test";
+            string str3 = str1;
+
+            Assert.Equal(str1.ReferenceEqualsExt(str1), ReferenceEquals(str1, str1));
+            Assert.Equal(str1.ReferenceEqualsExt(str2), ReferenceEquals(str1, str2));
+            Assert.Equal(str1.ReferenceEqualsExt(str3), ReferenceEquals(str1, str3));
+        }
+
+        [Fact]
+        public void ToInt32()
+        {
+            string one = "1";
+            Assert.Equal(one.ToInt32Ext(), Convert.ToInt32(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToInt32Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = Int64.MaxValue.ToString();
+            Assert.Throws<OverflowException>(() => three.ToInt32Ext());
+        }
+
+        [Fact]
+        public void ToUInt32()
+        {
+            string one = "1";
+            Assert.Equal(one.ToUInt32Ext(), Convert.ToUInt32(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToUInt32Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = "-3";
+            Assert.Throws<OverflowException>(() => three.ToUInt32Ext());
+        }
+
+        [Fact]
+        public void ToInt16()
+        {
+            string one = "1";
+            Assert.Equal(one.ToInt16Ext(), Convert.ToInt16(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToInt16Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = int.MaxValue.ToString();
+            Assert.Throws<OverflowException>(() => three.ToInt16Ext());
+        }
+
+        [Fact]
+        public void ToUInt16()
+        {
+            string one = "1";
+            Assert.Equal(one.ToUInt16Ext(), Convert.ToUInt16(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToUInt16Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = "-3";
+            Assert.Throws<OverflowException>(() => three.ToUInt16Ext());
+        }
+
+        [Fact]
+        public void ToIn64()
+        {
+            string one = "1";
+            Assert.Equal(one.ToInt64Ext(), Convert.ToInt64(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToInt64Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+        }
+
+        [Fact]
+        public void ToUInt64()
+        {
+            string one = "1";
+            Assert.Equal(one.ToUInt64Ext(), Convert.ToUInt64(one));
+
+            string two = "two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToUInt64Ext());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = "-3";
+            Assert.Throws<OverflowException>(() => three.ToUInt64Ext());
+        }
+
+        [Fact]
+        public void ToDouble()
+        {
+            string one = "1.5";
+            Assert.Equal(one.ToDoubleExt(), Convert.ToDouble(one));
+
+            string two = "two point two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToDoubleExt());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+        }
+
+        [Fact]
+        public void ToSingle()
+        {
+            string one = "1.5";
+            Assert.Equal(one.ToSingleExt(), Convert.ToSingle(one));
+
+            string two = "two point two";
+            FormatException formatEx = Assert.Throws<FormatException>(() => two.ToSingleExt());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+
+            string three = Double.MaxValue.ToString();
+            OverflowException overflowEx = Assert.Throws<OverflowException>(() => three.ToSingleExt());
+            Assert.Equal("Value was either too large or too small for a Single.", overflowEx.Message);
+        }
+
+        [Fact]
+        public void ToBoolean()
+        {
+            string t = "true";
+
+            Assert.True(t.ToBooleanExt());
+            Assert.Equal(t.ToBooleanExt(), Convert.ToBoolean(t));
+
+
+            string t2 = "is true";
+            FormatException formatEx = Assert.Throws<FormatException>(() => t2.ToBooleanExt());
+            Assert.Equal("String was not recognized as a valid Boolean.", formatEx.Message);
+        }
+
+        [Fact]
+        public void ToByte()
+        {
+            string b = "1";
+            Assert.Equal(b.ToByteExt(), Convert.ToByte(b));
+
+            string b2 = "test";
+            FormatException formatEx = Assert.Throws<FormatException>(() => b2.ToByteExt());
+            Assert.Equal("Input string was not in a correct format.", formatEx.Message);
+        }
+
+        [Fact]
+        public void ToChar()
+        {
+            string c = "c";
+            Assert.Equal(c.ToCharExt(), Convert.ToChar(c));
+
+            string c2 = "cc";
+            FormatException formatEx = Assert.Throws<FormatException>(() => c2.ToCharExt());
+            Assert.Equal("String must be exactly one character long.", formatEx.Message);
+        }
+
+        [Fact]
+        public void ToDateTime()
+        {
+            string date = "01/01/2018";
+            Assert.Equal(date.ToDateTimeExt(), Convert.ToDateTime(date));
+
+            string date2 = "January 1, 2017";
+            DateTime date3 = Convert.ToDateTime(date2);
+            Assert.Equal(date2.ToDateTimeExt(), date3);
+
+            string date4 = "This is not a date";
+            Assert.Throws<FormatException>(() => date4.ToDateTimeExt());
+        }
+
+        [Fact]
+        public void ToDecimal()
+        {
+            string d = "1.25";
+            Assert.Equal(d.ToDecimalExt(), Convert.ToDecimal(d));
+
+            string d2 = "one point two five";
+            Assert.Throws<FormatException>(() => d2.ToDecimalExt());
+        }
+
+        [Fact]
+        public void ToSByte()
+        {
+            string sb = "100";
+            Assert.Equal(sb.ToSByteExt(), Convert.ToSByte(sb));
+
+            string sb2 = "1000";
+            Assert.Throws<OverflowException>(() => sb2.ToSByteExt());
+        }
+
+        [Fact]
+        public void ToFloat()
+        {
+            string one = "1.4";
+            float f = 1.4F;
+            Assert.Equal(one.ToFloatExt(), f);
+
+            string two = "two point four";
+            Assert.Throws<FormatException>(() => two.ToFloatExt());
+        }
+
+        [Fact]
+        public void ToLong()
+        {
+            string one = "100";
+            long l = 100;
+            Assert.Equal(one.ToLongExt(), l);
+
+            string two = "two";
+            Assert.Throws<FormatException>(() => two.ToLongExt());
+        }
+
+        [Fact]
+        public void ToShort()
+        {
+            string one = "100";
+            short s = 100;
+            Assert.Equal(one.ToShortExt(), s);
+
+            string two = "two";
+            Assert.Throws<FormatException>(() => two.ToShortExt());
+
+            string three = long.MaxValue.ToString();
+            Assert.Throws<FormatException>(() => three.ToShortExt());
+        }
+    }
+}
---
+++ Extensions.net/StringExtensions.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Text;
+
+namespace Extensions.net.strings
+{
+    /// <summary>
+    /// String extensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Maps to string.IsNullOrWhiteSpace
+        /// </summary>
+        /// <returns><c>true</c>, if string null or white space, <c>false</c> otherwise.</returns>
+        /// <param name="text">Text.</param>
+        public static bool IsNullOrWhiteSpaceExt(this String text)
+        {
+            return string.IsNullOrWhiteSpace(text);
+        }
+
+        /// <summary>
+        /// maps to string.IsNullOrEmpty
+        /// </summary>
+        /// <returns><c>true</c>, if string null or empty, <c>false</c> otherwise.</returns>
+        /// <param name="text">Text.</param>
+        public static bool IsNullOrEmptyExt(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+        /// <summary>
+        /// Maps to string.<paramref name="compareTo"/>, sets the String comparison to Invariant Culture Ignore Case.
+        /// </summary>
+        /// <returns>The value that comes first alphabeltically. If the same returns an empty string. Ignores case.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="compareTo">Compare to.</param>
+        public static int CompareExt(this string text, string compareTo)
+        {
+            return string.Compare(text, compareTo, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        /// <summary>
+        /// Returns the first element alphabetically. Of they are the same, returns an empty string.
+        /// </summary>
+        /// <returns>The first ext.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="compareTo">Compare to.</param>
+        public static string CompareFirstExt(this string text, string compareTo)
+        {
+            int res = string.Compare(text, compareTo, StringComparison.InvariantCultureIgnoreCase);
+            if (res < 0) return text;
+            if (res > 0) return compareTo;
+
+            return "";
+        }
+
+        /// <summary>
+        /// maps to string.CompareOrdinal
+        /// </summary>
+        /// <returns>integer indicating which comes first in the sort order.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="compareTo">Compare to.</param>
+        public static int CompareOrdinalExt(this string text, string compareTo)
+        {
+            return string.CompareOrdinal(text, compareTo);
+        }
+
+        /// <summary>
+        /// Maps to string.CompareOrdinal
+        /// </summary>
+        /// <returns>The ordinal.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="textIndex">Text index.</param>
+        /// <param name="compareTo">Compare to.</param>
+        /// <param name="compareToIndex">Compare to index.</param>
+        /// <param name="length">Length.</param>
+        public static int CompareOrdinalExt(this string text, int textIndex, string compareTo, int compareToIndex, int length)
+        {
+            return string.CompareOrdinal(text, textIndex, compareTo, compareToIndex, length);
+        }
+
+        /// <summary>
+        /// Concatenate the specified text and n number of strings. Can be a list of strings or an array
+        /// Similar to string.Concat.
+        /// </summary>
+        /// <returns>The concatenate.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="toAdd">To add.</param>
+        public static string ConcatExt(this string text, params string[] toAdd)
+        {
+            if (toAdd != null)
+            {
+                StringBuilder sb = new StringBuilder(text);
+                for (int i = 0; i < toAdd.Length; i++)
+                {
+                    sb.Append(toAdd[i]);
+                }
+
+                return sb.ToString();
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        /// <summary>
+        /// Maps to string.Copy.
+        /// </summary>
+        /// <returns>The copy.</returns>
+        /// <param name="text">Text.</param>
+        public static string CopyExt(this string text)
+        {
+            return string.Copy(text);
+        }
+
+        /// <summary>
+        /// Maps to string.Format
+        /// </summary>
+        /// <returns>The formatted.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="values">Values.</param>
+        public static string FormatExt(this string text, params object[] values)
+        {
+            if (values != null)
+            {
+                return string.Format(text, values);
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        /// <summary>
+        /// Maps to string.Intern
+        /// </summary>
+        /// <returns>The intern.</returns>
+        /// <param name="text">Text.</param>
+        public static string InternExt(this string text)
+        {
+            return string.Intern(text);
+        }
+
+        /// <summary>
+        /// Maps to string.IsInterned
+        /// </summary>
+        /// <returns>The intern.</returns>
+        /// <param name="text">Text.</param>
+        public static string IsInternExt(this string text)
+        {
+            return string.IsInterned(text);
+        }
+
+        /// <summary>
+        /// Maps to string.Join
+        /// </summary>
+        /// <returns>The ext.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="separator">Separator.</param>
+        /// <param name="value">Value.</param>
+        /// <param name="startIndex">Start index.</param>
+        /// <param name="count">Count.</param>
+        public static string JoinExt(this string text, string separator, string[] value, int startIndex, int count)
+        {
+            string[] newArr = new string[value.Length + 1];
+            newArr[0] = text;
+            value.CopyTo(newArr, 1);
+            return string.Join(separator, newArr, startIndex, count);
+        }
+
+        /// <summary>
+        /// Maps to string.Join
+        /// </summary>
+        /// <returns>The ext.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="separator">Separator.</param>
+        /// <param name="value">Value.</param>
+        public static string JoinExt(this string text, string separator, params string[] value)
+        {
+            string[] newArr = new string[value.Length + 1];
+            newArr[0] = text;
+            value.CopyTo(newArr, 1);
+            return string.Join(separator, newArr);
+        }
+
+        /// <summary>
+        /// Maps to string.ReferenceEquals
+        /// </summary>
+        /// <returns><c>true</c>, if equals ext was referenced, <c>false</c> otherwise.</returns>
+        /// <param name="text">Text.</param>
+        /// <param name="compareTo">Compare to.</param>
+        public static bool ReferenceEqualsExt(this string text, string compareTo)
+        {
+            return ReferenceEquals(text, compareTo);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToInt32
+        /// </summary>
+        /// <returns>The int32 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static int ToInt32Ext(this string text)
+        {
+            return Convert.ToInt32(text);
+        }
+
+        /// <summary>
+        /// Maps to Covert.ToUInt32
+        /// </summary>
+        /// <returns>The user interface nt32 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static uint ToUInt32Ext(this string text)
+        {
+            return Convert.ToUInt32(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToInt16
+        /// </summary>
+        /// <returns>The int16 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static Int16 ToInt16Ext(this string text)
+        {
+            return Convert.ToInt16(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToUInt16
+        /// </summary>
+        /// <returns>The user interface nt16 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static UInt16 ToUInt16Ext(this string text)
+        {
+            return Convert.ToUInt16(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToInt64
+        /// </summary>
+        /// <returns>The int64 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static Int64 ToInt64Ext(this string text)
+        {
+            return Convert.ToInt64(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToUInt64
+        /// </summary>
+        /// <returns>The user interface nt64 ext.</returns>
+        /// <param name="text">Text.</param>
+        public static UInt64 ToUInt64Ext(this string text)
+        {
+            return Convert.ToUInt64(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToDouble
+        /// </summary>
+        /// <returns>The double ext.</returns>
+        /// <param name="text">Text.</param>
+        public static double ToDoubleExt(this string text)
+        {
+            return Convert.ToDouble(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToSingle
+        /// </summary>
+        /// <returns>The single ext.</returns>
+        /// <param name="text">Text.</param>
+        public static Single ToSingleExt(this string text)
+        {
+            return Convert.ToSingle(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToBoolean
+        /// </summary>
+        /// <returns><c>true</c>, if boolean ext was toed, <c>false</c> otherwise.</returns>
+        /// <param name="text">Text.</param>
+        public static bool ToBooleanExt(this string text)
+        {
+            return Convert.ToBoolean(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToByte
+        /// </summary>
+        /// <returns>The byte ext.</returns>
+        /// <param name="text">Text.</param>
+        public static byte ToByteExt(this string text)
+        {
+            return Convert.ToByte(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToChar
+        /// </summary>
+        /// <returns>The char ext.</returns>
+        /// <param name="text">Text.</param>
+        public static char ToCharExt(this string text)
+        {
+            return Convert.ToChar(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToDateTime;
+        /// </summary>
+        /// <returns>The date time ext.</returns>
+        /// <param name="text">Text.</param>
+        public static DateTime ToDateTimeExt(this string text)
+        {
+            return Convert.ToDateTime(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToDecimal
+        /// </summary>
+        /// <returns>The decimal ext.</returns>
+        /// <param name="text">Text.</param>
+        public static decimal ToDecimalExt(this string text)
+        {
+            return Convert.ToDecimal(text);
+        }
+
+        /// <summary>
+        /// Maps to Convert.ToSByte.
+        /// </summary>
+        /// <returns>The SB yte ext.</returns>
+        /// <param name="text">Text.</param>
+        public static SByte ToSByteExt(this string text)
+        {
+            return Convert.ToSByte(text);
+        }
+
+        /// <summary>
+        /// Converts a string to a float or throws a format exception.
+        /// </summary>
+        /// <returns>The float ext.</returns>
+        /// <param name="text">Text.</param>
+        public static float ToFloatExt(this string text)
+        {
+            float f = -1;
+            if (float.TryParse(text, out f))
+            {
+                return f;
+            }
+            else
+            {
+                throw new FormatException("Input string was not in a correct format.");
+            }
+        }
+
+        /// <summary>
+        /// Converts a string to a long or throws a Format Exception
+        /// </summary>
+        /// <returns>The long ext.</returns>
+        /// <param name="text">Text.</param>
+        public static long ToLongExt(this string text)
+        {
+            long l = -1;
+            if (long.TryParse(text, out l))
+            {
+                return l;
+            }
+            else
+            {
+                throw new FormatException("Input string was not in a correct format.");
+            }
+        }
+
+        /// <summary>
+        /// Converts a string to a short or throws a format exception.
+        /// </summary>
+        /// <returns>The short ext.</returns>
+        /// <param name="text">Text.</param>
+        public static short ToShortExt(this string text)
+        {
+            short s = -1;
+            if (short.TryParse(text, out s))
+            {
+                return s;
+            }
+            else
+            {
+                throw new FormatException("Input string was not in a correct format.");
+            }
+        }
+    }
+}
