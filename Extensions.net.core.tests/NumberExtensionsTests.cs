246f7f910395be46474b227f211ab1151dc69bbf
Adrian <adyle5@hotmail.com>
2018-10-16 14:11:59 -0400

    Add new numeric extensions.

--- Extensions.net.core.tests/NumberExtensionsTests.cs
+++ Extensions.net.core.tests/NumberExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Extensions.net.core.tests
 {
@@ -16,6 +17,161 @@ namespace Extensions.net.core.tests
             output = 0;
             output.TryParseExt(input2);
             Assert.Equal(0, output);
+        }
+
+        [Fact]
+        public void DateTime()
+        {
+            long l = 123456789;
+            DateTime d = new DateTime(l);
+            Assert.Equal(d, l.ToDateTimeExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneInt()
+        {
+            int? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            int? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            int? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+
+        [Fact]
+        public void IsNullOrLessThanOneInt16()
+        {
+            Int16? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            Int16? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            Int16? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneInt64()
+        {
+            Int64? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            Int64? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            Int64? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneUInt32()
+        {
+            UInt32? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            UInt32? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            UInt32? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneUInt16()
+        {
+            UInt16? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            UInt16? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            UInt16? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneUInt64()
+        {
+            UInt64? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            UInt64? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            UInt64? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneDouble()
+        {
+            double? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            double? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            double? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+
+            float? f = 0f;
+            Assert.True(f.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneSingle()
+        {
+            Single? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            Single? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            Single? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneDecimal()
+        {
+            decimal? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            decimal? i2 = 2m;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            decimal? i3 = 0m;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneByte()
+        {
+            byte? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            byte? i2 = 1;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            byte? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
+        }
+
+        [Fact]
+        public void IsNullOrLessThanOneSByte()
+        {
+            byte? i = null;
+            Assert.True(i.IsNullOrLessThanOneExt());
+
+            byte? i2 = 2;
+            Assert.False(i2.IsNullOrLessThanOneExt());
+
+            byte? i3 = 0;
+            Assert.True(i3.IsNullOrLessThanOneExt());
         }
     }
 }
--- Extensions.net/NumberExtensions.cs
+++ Extensions.net/NumberExtensions.cs
@@ -23,5 +23,89 @@ namespace Extensions.net
         /// <param name="output"></param>
         /// <param name="input"></param>
         public static void ParseExt(this ref int output, string input) => output = int.Parse(input);
+
+        /// <summary>
+        /// Returns a DateTime object from ticks.
+        /// </summary>
+        /// <param name="ticks"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTimeExt(this long ticks) => new DateTime(ticks);
+
+        /// <summary>
+        /// Checks if a nullable integer is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this int? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable Int16 is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this Int16? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable Int64 is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this Int64? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable UInt32 is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this UInt32? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable UInt16 is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this UInt16? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable UInt64 is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this UInt64? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable double is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this double? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable Single is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this Single? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable decimal is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this decimal? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable byte is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this byte? num) => num == null || num < 1;
+
+        /// <summary>
+        /// Checks if a nullable sbyte is null or is less than one.
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool IsNullOrLessThanOneExt(this sbyte? num) => num == null || num < 1;
     }
 }
